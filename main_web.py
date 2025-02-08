b2b9d99831446128c912c855cbe229975f6f5966
TeenyDesert9892 <98417678+TeenyDesert9892@users.noreply.github.com>
2025-02-08 10:25:11 +0100

    V0.8.0

--- main_web.py
+++ main_web.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
+    import webbrowser
     import flet as ft
     
     from scripts.configHandeler import configHandeler
@@ -21,5 +22,6 @@ if __name__ == '__main__':
     
     from scripts import mainGui as MainGui
     
+    webbrowser.open("127.0.0.1:9892")
     ft.app(MainGui.gui, assets_dir=".", view=None, port=9892, host="0.0.0.0")
     ConfigHandeler.save_config()
